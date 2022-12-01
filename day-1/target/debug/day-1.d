8c883e71af094b0ca3742630cbb8c7faf48a7d58
Tech-Matt <mattia.rizzo.un@gmail.com>
2022-12-01 17:50:48 +0100

    day 1 ex: 1

---
+++ day-1/target/debug/day-1.d
@@ -0,0 +1,1 @@
+/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/target/debug/day-1: /home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/src/main.rs
---
+++ day-1/target/debug/deps/day_1-40f6c3fc98ec49b6.d
@@ -0,0 +1,5 @@
+/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/target/debug/deps/day_1-40f6c3fc98ec49b6: src/main.rs
+
+/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/target/debug/deps/day_1-40f6c3fc98ec49b6.d: src/main.rs
+
+src/main.rs:
