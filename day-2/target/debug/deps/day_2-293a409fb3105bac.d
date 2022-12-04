c7267152c22e8bd2c0202540c2aef23684a6a47a
Mattia <mattia.rizzo.un@gmail.com>
2022-12-04 01:06:08 +0100

    Delete day-2 directory

--- day-2/target/debug/deps/day_2-293a409fb3105bac.d
+++
@@ -1,5 +0,0 @@
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-2/target/debug/deps/day_2-293a409fb3105bac.rmeta: src/main.rs
-
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-2/target/debug/deps/day_2-293a409fb3105bac.d: src/main.rs
-
-src/main.rs:
--- day-2/target/debug/deps/day_2-6efbab4ff771e30c.d
+++
@@ -1,5 +0,0 @@
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-2/target/debug/deps/day_2-6efbab4ff771e30c.rmeta: src/main.rs
-
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-2/target/debug/deps/day_2-6efbab4ff771e30c.d: src/main.rs
-
-src/main.rs:
