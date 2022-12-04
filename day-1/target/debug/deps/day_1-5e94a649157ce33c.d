5ae9dff934f8bd99a14678a5fa5c6c422419f460
Mattia <mattia.rizzo.un@gmail.com>
2022-12-04 01:05:56 +0100

    Delete day-1 directory

--- day-1/target/debug/deps/day_1-5e94a649157ce33c.d
+++
@@ -1,5 +0,0 @@
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/target/debug/deps/day_1-5e94a649157ce33c.rmeta: src/main.rs
-
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/target/debug/deps/day_1-5e94a649157ce33c.d: src/main.rs
-
-src/main.rs:
--- day-1/target/debug/deps/day_1-b4d9ff032ccff7dd.d
+++
@@ -1,5 +0,0 @@
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/target/debug/deps/day_1-b4d9ff032ccff7dd.rmeta: src/main.rs
-
-/home/techmatt01/Documents/Programming/Rust/advent-of-code-22/day-1/target/debug/deps/day_1-b4d9ff032ccff7dd.d: src/main.rs
-
-src/main.rs:
