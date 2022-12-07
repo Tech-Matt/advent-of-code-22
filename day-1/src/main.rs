23deac4ad3dfcd09e915eee746cdcbc8191574c4
Tech-Matt <mattia.rizzo.un@gmail.com>
2022-12-08 00:22:19 +0100

    Completely destroyed everything and rebuilt it

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -7,22 +7,18 @@
  * Problem 1: Find the elf carrying the most calories. Return calories value of that elf
  * Problem 2: Find top 3 elves and return sum
  */
-
-
-
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io::BufReader;
+use common::fileio;
 
 fn main() {
-   
+
     let mut max_sum :u32 = 0;
     let mut sum_vec = Vec::new();
     //Importing file
     if let Ok(data) = read_lines("data.txt") {
-         let mut partial_sum: u32 = 0;
+        let mut partial_sum: u32 = 0;
 
-         for line in data {
+        for line in data {
             //If it's a blank line i will update max_sum and reset partial_sum
             if line.as_ref().unwrap().is_empty() {
                 if partial_sum > max_sum {
@@ -41,23 +37,16 @@ fn main() {
             partial_sum += line.unwrap().parse::<u32>().unwrap();
 
             //println!("{}", line.unwrap());
-         }
+        }
 
-         //Get the sum of top 3 elves
-         sum_vec.sort();
-         let len = sum_vec.len();
-         let top3_sum: u32 = sum_vec[len - 1] + sum_vec[len - 2] + sum_vec[len-3];
+        //Get the sum of top 3 elves
+        sum_vec.sort();
+        let len = sum_vec.len();
+        let top3_sum: u32 = sum_vec[len - 1] + sum_vec[len - 2] + sum_vec[len-3];
 
 
-         println!("N° of Max calories is: {}", max_sum);
-         println!("N° of Top 3 calories is: {}", top3_sum);
+        println!("N° of Max calories is: {}", max_sum);
+        println!("N° of Top 3 calories is: {}", top3_sum);
     }
-    
-}
 
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,6 +1,27 @@
+//ROCK, PAPER, SCISSOR TOURNAMENT
+/*
+-Rock defeats scissors, scissors defeat paper, paper defeat rock
+-If they choose the same shape, it's a draw
+
+INPUT FILE:
+- 1° Column: Shape of opponent (A for rock, B for paper, C for scissor
+- 2° column: what you should reply to win, X for rock, Y for paper, Z for scissors
+
+OUTPUT:
+total_score: sum of your scores for each round
+
+score for a single round = shape you selected + outcome of round
+
+where _shape you selected_ = (1: Rock, 2: paper, 3: scissor)
+outcome = (0: Lost, 3: Draw, 6: Won)
+
+return total_score
+
+*/
+
+
+
 fn main()
 {
-
-	println!("Hello World");
 
 }
